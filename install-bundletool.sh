4c724b6f73e160d6e08c88a436f335c86f7c3fe7
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-01-10 21:19:29 +0530

    Update install-bundletool.sh

--- install-bundletool.sh
+++ install-bundletool.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 
-# This shell script is meant to be sourced
-
 set -e
 
-install_bundletool() {
-  declare -r version=$( \
-    curl -s "https://api.github.com/repos/google/bundletool/releases/latest" \
-    | jq .name -r)
-  curl -o "$ANDROID_SDK_ROOT/bundletool-all.jar" \
-    -sL "https://github.com/google/bundletool/releases/download/$version/bundletool-all-$version.jar"
-}
-
-install_bundletool
-# shellcheck disable=SC2139
-alias bundletool="java -jar $ANDROID_SDK_ROOT/bundletool-all.jar"
-
-set +e
+VERSION=$( \
+  curl -s "https://api.github.com/repos/google/bundletool/releases/latest" \
+  | jq .name -r)
+curl -o "$ANDROID_SDK_ROOT/bundletool-all.jar" \
+  -sL "https://github.com/google/bundletool/releases/download/$VERSION/bundletool-all-$VERSION.jar"
