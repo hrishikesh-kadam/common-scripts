2af3064ecf46c28c5a3834a11b0eb36620354e1b
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-01-25 02:41:50 +0530

    Update install-bundletool.sh

--- android/install-bundletool.sh
+++ android/install-bundletool.sh
@@ -6,8 +6,8 @@ VERSION=$( \
   curl -s "https://api.github.com/repos/google/bundletool/releases/latest" \
     | jq -r .name
 )
-curl -o "$ANDROID_HOME/bundletool-all.jar" \
+BUNDLETOOL_PATH="$ANDROID_HOME/bundletool-all.jar"
+curl -o "$BUNDLETOOL_PATH" \
   -sL "https://github.com/google/bundletool/releases/download/$VERSION/bundletool-all-$VERSION.jar"
-
 printf "bundletool "
-java -jar "$ANDROID_HOME/bundletool-all.jar" version
+java -jar "$BUNDLETOOL_PATH" version
