e59081222a0de8079a658c0da4dbbfc102f2c75c
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-05-24 23:03:47 +0530

    Update install-android-cmd-tools.sh for other OS

--- android/install-android-cmd-tools.sh
+++ android/install-android-cmd-tools.sh
@@ -2,11 +2,23 @@
 
 set -e -o pipefail
 
+KERNEL_NAME=$(uname -s)
+if [[ $KERNEL_NAME =~ ^"MINGW" ]]; then
+  OS_SHORT="win"
+elif [[ $KERNEL_NAME =~ ^"Darwin" ]]; then
+  OS_SHORT="mac"
+elif [[ $KERNEL_NAME =~ ^"Linux" ]]; then
+  OS_SHORT="linux"
+else
+  printf "%b\n" "\033[31mError: Unknown Kernel - $KERNEL_NAME\033[0m"
+  exit 1
+fi
+
 FILE_LINK=$(
   curl -s https://developer.android.com/studio#command-tools \
-    | grep -o "https://dl.google.com/android/repository/commandlinetools-linux-[0-9]*_latest.zip"
+    | grep -o "https://dl.google.com/android/repository/commandlinetools-$OS_SHORT-[0-9]*_latest.zip"
 )
-FILE_NAME="$(echo "$FILE_LINK" | grep -o "commandlinetools-linux-[0-9]*_latest")"
+FILE_NAME="$(echo "$FILE_LINK" | grep -o "commandlinetools-$OS_SHORT-[0-9]*_latest")"
 curl -o "/tmp/android-$FILE_NAME.zip" \
   -sL "$FILE_LINK"
 unzip -qo "/tmp/android-$FILE_NAME.zip" "cmdline-tools/*" \
