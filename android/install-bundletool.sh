6e9d1f1cb27a20ba1a91a446a4e47729e6735000
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-06-03 00:53:21 +0530

    Fix GitHub API rate limiting

--- android/install-bundletool.sh
+++ android/install-bundletool.sh
@@ -2,12 +2,25 @@
 
 set -e -o pipefail
 
-VERSION=$( \
-  curl -s "https://api.github.com/repos/google/bundletool/releases/latest" \
-    | jq -r .name
-)
+if [[ $GITHUB_ACTIONS == "true" ]]; then
+  # To avoid GitHub API rate limiting
+  VERSION=$( \
+    curl --silent --show-error --location --fail \
+      --output /dev/null \
+      --write-out "%{url_effective}" \
+      https://github.com/google/bundletool/releases/latest \
+      | xargs basename
+  )
+else
+  VERSION=$( \
+    curl --silent --show-error --location --fail \
+      "https://api.github.com/repos/google/bundletool/releases/latest" \
+      | jq -r .name
+  )
+fi
 BUNDLETOOL_PATH="$ANDROID_HOME/bundletool-all.jar"
-curl -o "$BUNDLETOOL_PATH" \
-  -sL "https://github.com/google/bundletool/releases/download/$VERSION/bundletool-all-$VERSION.jar"
+curl --silent --show-error --location --fail \
+  --output "$BUNDLETOOL_PATH" \
+  "https://github.com/google/bundletool/releases/download/$VERSION/bundletool-all-$VERSION.jar"
 printf "bundletool "
 java -jar "$BUNDLETOOL_PATH" version
