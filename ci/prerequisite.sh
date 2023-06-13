3a305499aeb9a46f9a9758c99e513ad0fc9038ee
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-06-13 21:52:20 +0530

    Add status logs in prerequisite.sh

--- ci/prerequisite.sh
+++ ci/prerequisite.sh
@@ -2,6 +2,10 @@
 
 set -e -o pipefail
 
+source "$COMMON_SCRIPTS_ROOT/logs/logs-env-bash.sh"
+
+echo "Checking prerequisite"
+
 "$COMMON_SCRIPTS_ROOT/ci/common-prerequisite.sh"
 
 if [[ $(uname -s) =~ ^"MINGW" ]]; then
@@ -12,3 +16,5 @@ COMMON_SCRIPTS_PREREQUISITE="done"
 if [[ $GITHUB_ACTIONS == "true" ]]; then
   echo "COMMON_SCRIPTS_PREREQUISITE=$COMMON_SCRIPTS_PREREQUISITE" >> "$GITHUB_ENV"
 fi
+
+print_in_green "Checked prerequisite"
