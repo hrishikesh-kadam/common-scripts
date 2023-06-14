397972e63aadab9f8d6654b9c2e0b933a2db4b37
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-06-15 01:06:16 +0530

    s/Powershell/PowerShell

--- logs/logs-env.ps1
+++ logs/logs-env.ps1
@@ -1,6 +1,6 @@
 Set-StrictMode -Version latest
 
-# `e was added in Powershell 6, so using  directly instead
+# `e was added in PowerShell 6, so using  directly instead
 
 function Write-InRed {
   Write-Output "[31m$args[0m"
